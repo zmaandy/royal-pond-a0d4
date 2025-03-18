16261de0a41697b1b3b098a21fba9bf9060a387b
zmaandy <137860241+zmaandy@users.noreply.github.com>
2025-03-18 13:49:10 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { renderHtml } from "./renderHtml";
 
 export default {
   async fetch(request, env) {
-    const stmt = env.DB.prepare("SELECT * FROM comments LIMIT 3");
+    const stmt = env.DB.prepare("INSERT INTO comments (author, content) VALUES ('John', 'Test')");
     const { results } = await stmt.all();
 
     return new Response(renderHtml(JSON.stringify(results, null, 2)), {
